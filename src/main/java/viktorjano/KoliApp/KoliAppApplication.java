c8d0e60484ded4eeac173868c7d653a95591b5c2
janekreitter <janekreitter@gmail.com>
2020-10-11 15:39:13 +0200

    first commit

---
+++ src/main/java/viktorjano/KoliApp/KoliAppApplication.java
@@ -0,0 +1,14 @@
+package viktorjano.KoliApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoliAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoliAppApplication.class, args);
+		System.out.println("asd");
+	}
+
+}
