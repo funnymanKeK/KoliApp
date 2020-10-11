c65de53ce33424106a6e18b9de7346aa0f6d06d4
janekreitter <janekreitter@gmail.com>
2020-10-11 15:39:37 +0200

    secound commit

---
+++ src/test/java/viktorjano/KoliApp/KoliAppApplicationTests.java
@@ -0,0 +1,13 @@
+package viktorjano.KoliApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoliAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
