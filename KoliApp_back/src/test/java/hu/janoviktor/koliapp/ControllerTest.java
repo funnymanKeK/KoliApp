84bb074944c90286666c7c082ec766cad7054ee6
janek <janek@DESKTOP-VTHV7TP>
2021-01-04 20:51:33 +0100

    backend tesztek és doksi kiegészítés

--- KoliApp_back/src/test/java/hu/janoviktor/koliapp/ControllerTest.java
+++ KoliApp_back/src/test/java/hu/janoviktor/koliapp/ControllerTest.java
@@ -2,6 +2,7 @@ package hu.janoviktor.koliapp;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -14,58 +15,130 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import hu.janoviktor.koliapp.dto.CommentDto;
 import hu.janoviktor.koliapp.dto.PostDto;
 import hu.janoviktor.koliapp.dto.ScheduleDto;
+import hu.janoviktor.koliapp.entity.Comment;
+import hu.janoviktor.koliapp.entity.Post;
 import hu.janoviktor.koliapp.entity.Room;
+import hu.janoviktor.koliapp.entity.User;
+import hu.janoviktor.koliapp.repository.PostRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTest {
 
-	  @LocalServerPort
-	  private int port;
-	
-	  @Autowired
-	  private TestRestTemplate restTemplate;
-	  
-	  @Test
-	  public void shouldReturnAllPosts() throws Exception {
-	    ResponseEntity<List<PostDto>> response = restTemplate.exchange("http://localhost:" + port + "/post", HttpMethod.GET, null, new ParameterizedTypeReference<List<PostDto>>() {});
-	    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	    assertThat(response.getBody().size()).isEqualTo(1);
-	  }
-	  
-	  @Test
-	  public void shouldReturnAllRooms() throws Exception {
-	    ResponseEntity<List<Room>> response = restTemplate.exchange("http://localhost:" + port + "/room", HttpMethod.GET, null, new ParameterizedTypeReference<List<Room>>() {});
-	    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	    assertThat(response.getBody().size()).isEqualTo(1);
-	  }
-	  
-	  @Test
-	  public void shouldReturnTrue() throws Exception {
-		  
-		 ScheduleDto scheduleDto = new ScheduleDto();
-		 scheduleDto.setUserId(1);
-		 scheduleDto.setRoomId(1);
-		 scheduleDto.setFromDate("1996/06/06-9:30");
-		 scheduleDto.setToDate("1996/06/06-10:30");
-		  
-	    ResponseEntity<Boolean> response = restTemplate.postForEntity("http://localhost:" + port + "/schedule/check", scheduleDto, Boolean.class );
-	    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	    assertThat(response.getBody().booleanValue()).isEqualTo(true);
-	  }
-	  
-	  @Test
-	  public void shouldReturnFalse() throws Exception {
-		  
-		 ScheduleDto scheduleDto = new ScheduleDto();
-		 scheduleDto.setUserId(1);
-		 scheduleDto.setRoomId(1);
-		 scheduleDto.setFromDate("1997/06/06-11:30");
-		 scheduleDto.setToDate("1997/06/06-13:30");
-		  
-	    ResponseEntity<Boolean> response = restTemplate.postForEntity("http://localhost:" + port + "/schedule/check", scheduleDto, Boolean.class );
-	    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	    assertThat(response.getBody().booleanValue()).isEqualTo(false);
-	  }
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@Autowired
+	private PostRepository postRepository;
+
+	@Test
+	public void shouldReturnAllPosts() throws Exception {
+		ResponseEntity<List<PostDto>> response = restTemplate.exchange("http://localhost:" + port + "/post",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<PostDto>>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().size()).isEqualTo(1);
+	}
+
+	@Test
+	public void shouldReturnAllRooms() throws Exception {
+		ResponseEntity<List<Room>> response = restTemplate.exchange("http://localhost:" + port + "/room",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Room>>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().size()).isEqualTo(1);
+	}
+
+	@Test
+	public void shouldReturnTrue() throws Exception {
+
+		ScheduleDto scheduleDto = new ScheduleDto();
+		scheduleDto.setUserId(1);
+		scheduleDto.setRoomId(1);
+		scheduleDto.setFromDate("1996/06/06-9:30");
+		scheduleDto.setToDate("1996/06/06-10:30");
+
+		ResponseEntity<Boolean> response = restTemplate.postForEntity("http://localhost:" + port + "/schedule/check",
+				scheduleDto, Boolean.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().booleanValue()).isEqualTo(true);
+	}
+
+	@Test
+	public void shouldReturnFalse() throws Exception {
+
+		ScheduleDto scheduleDto = new ScheduleDto();
+		scheduleDto.setUserId(1);
+		scheduleDto.setRoomId(1);
+		scheduleDto.setFromDate("1997/06/06-11:30");
+		scheduleDto.setToDate("1997/06/06-13:30");
+
+		ResponseEntity<Boolean> response = restTemplate.postForEntity("http://localhost:" + port + "/schedule/check",
+				scheduleDto, Boolean.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().booleanValue()).isEqualTo(false);
+	}
+
+	@Test
+	public void shouldCreateNewThenArchivePost() throws Exception {
+		Post post = postRepository.findById(new Long(2)).orElseThrow();
+		post.setArchive(false);
+		postRepository.save(post);
+		ResponseEntity<List<PostDto>> response = restTemplate.exchange("http://localhost:" + port + "/post",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<PostDto>>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().size()).isEqualTo(1);
+
+		post = postRepository.findById(new Long(2)).orElseThrow();
+		post.setArchive(true);
+		postRepository.save(post);
+		response = restTemplate.exchange("http://localhost:" + port + "/post", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<PostDto>>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().size()).isEqualTo(0);
+	}
+
+	@Test
+	public void likingPost() throws Exception {
+		Post post = postRepository.findById(new Long(2)).orElseThrow();
+		post.setLikes(new ArrayList<User>());
+		post.setArchive(false);
+		postRepository.save(post);
+		ResponseEntity<List<PostDto>> response = restTemplate.exchange("http://localhost:" + port + "/post",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<PostDto>>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().size()).isEqualTo(1);
+		assertThat(response.getBody().get(0).isCreatorLiked()).isEqualTo(false);
+
+		post = postRepository.findById(new Long(2)).orElseThrow();
+		List<User> likes = new ArrayList<User>();
+		likes.add(post.getUser());
+		post.setLikes(likes);
+		postRepository.save(post);
+		response = restTemplate.exchange("http://localhost:" + port + "/post", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<PostDto>>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().size()).isEqualTo(1);
+		assertThat(response.getBody().get(0).isCreatorLiked()).isEqualTo(true);
+	}
+
+	@Test
+	public void commentingPost() throws Exception {
+		CommentDto commentDto = new CommentDto();
+		commentDto.setPostId(2);
+		commentDto.setUserId(1);
+		commentDto.setText("teszt komment");
+		ResponseEntity<Comment> response = restTemplate.postForEntity("http://localhost:" + port + "/comment",
+				commentDto, Comment.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().getText()).isEqualTo("teszt komment");
+	}
 }
